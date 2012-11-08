5943cb0e618ab7e543f42fbe76ef5ddaf635de57
Alexzander thunder Shevchenko <thunder@blackdeath.ru>
2012-11-08 17:47:33 +0400

    added icon to пт сау

--- js/wot_tables.js
+++ js/wot_tables.js
@@ -1137,7 +1137,7 @@ $(function() {
 		height: "100%",
 		rowNum: 100,
 		hiddengrid:true,
-		caption: '<img src="images/icons/level-ico.png"/> ПТ-САУ',
+		caption: '<span class="b-encyclopedia-type b-encyclopedia-type_at-spg"/> ПТ-САУ',
 		rownumbers: false,
 		rownumWidth: 40,
 		grouping: false,
